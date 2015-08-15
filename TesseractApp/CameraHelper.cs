9971f238898c3c058c2264f79f8457d75ba3b6cf
Gareth Price <garethpriceza@gmail.com>
2015-08-15 15:14:30 +0200

    Initial check-in of module TesseractApp

---
+++ TesseractApp/CameraHelper.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Android.Content.PM;
+using Android.Content;
+using Android.Provider;
+using System.Collections.Generic;
+using Android.Util;
+using Java.IO;
+using Android.Graphics;
+
+namespace TesseractApp
+{
+	public class CameraHelper
+	{
+		public CameraHelper ()
+		{
+			CreatePictureDirectory ();
+		}
+
+		internal bool AppToTakePicturesExists(PackageManager pkgManager)
+		{
+			// Check if there is an application that can take pictures
+			var intent = new Intent (MediaStore.ActionImageCapture);
+			IList<ResolveInfo> availableActivities =  pkgManager.QueryIntentActivities (intent, PackageInfoFlags.MatchDefaultOnly);
+
+			foreach (var item in availableActivities) {
+
+				Log.Info ("AVAILABLE CAMERA APPS", item.ToString());
+			}
+
+			return availableActivities != null && availableActivities.Count > 0;
+		}
+
+		internal void CreatePictureDirectory()
+		{
+			// Set the phones DCIM/Camera directory
+			ImageInfo._dir = new File (Android.OS.Environment.GetExternalStoragePublicDirectory(Android.OS.Environment.DirectoryDcim),"Camera"); 
+
+			if (!ImageInfo._dir.Exists ()) {
+				ImageInfo._dir.Mkdirs ();
+				Log.Info ("Directory Made",ImageInfo._dir.ToString());
+			}
+
+			Log.Info ("Directory",ImageInfo._dir.ToString());
+		}
+
+		internal Intent LaunchCameraApp()
+		{
+			// Create the intent to launch the camera application
+			var intent = new Intent (MediaStore.ActionImageCapture);
+			ImageInfo._file = new File (ImageInfo._dir, string.Format ("{0}.jpg", Guid.NewGuid ()));
+			Log.Info ("FILE",ImageInfo._file.ToString());
+			intent.PutExtra (MediaStore.ExtraOutput, Android.Net.Uri.FromFile(ImageInfo._file));
+			return intent;
+		}
+	}
+
+	public static class ImageInfo
+	{
+		internal static File _file { get; set;}
+		internal static File _dir { get; set;}
+		internal static Bitmap _bitmap { get; set;}
+	}
+}
+
---
+++ TesseractApp/MainActivity.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Tesseract.Droid;
+using System.Threading.Tasks;
+using Android.Util;
+
+namespace TesseractApp
+{
+	[Activity (Label = "TesseractApp", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			var CH = new CameraHelper();
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button button = FindViewById<Button> (Resource.Id.myButton);
+			Button button1 = FindViewById<Button> (Resource.Id.myButton2);
+			
+			button.Click += delegate {
+
+				var pkgManager = PackageManager;
+
+				if(CH.AppToTakePicturesExists(pkgManager)){
+					StartActivityForResult(CH.LaunchCameraApp(),0);
+				}
+				else{
+
+					Toast.MakeText(
+						Application.Context,
+						"No Camera Application Found",
+						ToastLength.Short).Show();
+				}
+			};
+
+			// calling async method 1
+			button1.Click += async (sender, e) => {
+
+				Task<String> strResult = GetTextFromImage();
+				var result = await strResult;
+				Log.Info("DETECTED TEXT",result);
+			Toast.MakeText (Application.Context,result,ToastLength.Long).Show();
+			};
+
+			// calling async method 2
+			//button1.Click += GetTextFromImg;
+
+		}
+
+		protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+		{
+			base.OnActivityResult (requestCode, resultCode, data);
+
+			// Add the image to the photo gallery
+			var mediaScanIntent = new Intent (Intent.ActionMediaScannerScanFile);
+			Android.Net.Uri contentUri = Android.Net.Uri.FromFile (ImageInfo._file);
+			mediaScanIntent.SetData (contentUri);
+			SendBroadcast (mediaScanIntent);
+		} 
+
+		// async method 1
+		private async Task<String> GetTextFromImage()
+		{
+			var tessAPI = new TesseractApi (Application.Context);
+			await tessAPI.Init("eng");
+			//tessAPI.SetWhitelist ("123456789");
+			await tessAPI.SetImage (ImageInfo._file.ToString());
+			return tessAPI.Text;
+		}
+			
+		/* async method 2  using event handler
+		 * The method is marked async but returns void. This is typically only done for event
+		 * handlers otherwise you return a Task or Task<TResult>
+		 */
+		async void GetTextFromImg(object sender, EventArgs e)
+		{
+			var result = await GetTextFromImage ();
+			Log.Info("DETECTED TEXT",result);
+			Toast.MakeText (Application.Context,result,ToastLength.Long).Show();
+
+		}
+
+	}
+}
+
+
